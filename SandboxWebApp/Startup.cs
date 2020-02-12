6c55d36be4d5bacf4670422cf88ed90079dada11
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2020-02-12 23:22:46 +0100

    Run cleanup on web app

--- SandboxWebApp/Startup.cs
+++ SandboxWebApp/Startup.cs
@@ -42,7 +42,7 @@ namespace SandboxWebApp
                 app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
-            
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
