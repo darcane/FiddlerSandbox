e08f5c41a30f0afa6be8fa5dabaea7b394f86d3a
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2020-02-12 22:45:08 +0100

    Part 2 last steps

---
+++ SandboxWebApp/Pages/PartTwo/SessionList.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SandboxWebApp.Pages.PartTwo
+{
+    public class SessionListModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
