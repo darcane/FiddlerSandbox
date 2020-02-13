4eb4c534c5c3c46aad059406456b220f08db45bc
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2020-02-13 03:43:00 +0100

    Finalizing part 3

---
+++ SandboxWebApp/Pages/PartThree/NetTrace.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SandboxWebApp.Pages.PartThree
+{
+    public class NetTraceModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ SandboxWebApp/Pages/PartThree/Remote.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SandboxWebApp.Pages.PartThree
+{
+    public class RemoteModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
