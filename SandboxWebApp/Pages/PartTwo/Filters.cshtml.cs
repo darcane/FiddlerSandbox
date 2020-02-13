98fe865075eccec41a28f17912f0074cce52a5f9
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2020-02-13 03:01:18 +0100

    End of part 2

---
+++ SandboxWebApp/Pages/PartTwo/Filters.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SandboxWebApp.Pages.PartTwo
+{
+    public class FiltersModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
